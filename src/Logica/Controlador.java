6801b18d73a0cb6420d5161b40a0a29dc7615589
Guiga <Guiga@Guiga-PC>
2012-04-26 20:18:07 -0300

    US 02 - Classe Perfil adicionada

--- src/Logica/Controlador.java
+++ src/Logica/Controlador.java
@@ -10,8 +10,7 @@ import easyaccept.Facade;
 
 public class Controlador {
 	private FachadaLogica fachadaLogica = new FachadaLogica();
-	private boolean sistemaAberto = false;
-	private int idSessao;
+	
 	
 	public void criarUsuario(String login, String senha, String nome, String endereco, String email) throws Exception{
 		fachadaLogica.criarUsuario(login, senha, nome, endereco, email);
@@ -32,21 +31,7 @@ public class Controlador {
 	}
 	
 	public int abrirSessao(String login, String senha) throws Exception{
-		//TODO ESCOLHER ONDE ISSO VAI FICAR!
-		if (login == null || login.equals("")){
-			throw new EasyAcceptException("Login inválido");
-		}
-		
-		if (!fachadaLogica.isLoginCadastrado(login)){
-			throw new EasyAcceptException("Usuário inexistente");
-		}
-		
-		if (!fachadaLogica.isSenhaValida(login, senha)){
-			throw new EasyAcceptException("Login inválido");
-		}
-		sistemaAberto = true;
-		//idSessao = "sessao" + login.substring(0, 1).toUpperCase() + login.substring(1, login.length());
-		return idSessao;
+		return fachadaLogica.abrirSessao(login, senha);
 	}
 	
 	public String getAtributoUsuario(String login, String atributo) throws Exception{
@@ -55,10 +40,6 @@ public class Controlador {
 	
 	public String getAtributoCarona(int idCarona, String atributo) throws Exception{
 		return fachadaLogica.getAtributoCarona(idCarona, atributo);
-	}
-	
-	public void encerrarSistema(){
-		sistemaAberto = false;
 	}
 	
 	public String localizarCarona(String idCarona, String origem, String destino){
