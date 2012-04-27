39572ae67794891243386d8e2352c430e3dedf37
Guiga <Guiga@Guiga-PC>
2012-04-27 00:23:18 -0300

    US 02 - Tratando Exceções(corrigir idSessaoSingleton na classe Perfil)

---
+++ src/Logica/FachadaLogica.java
@@ -0,0 +1,71 @@
+package Logica;
+
+public class FachadaLogica {
+	private Usuario pessoa;
+	private FachadaDados fachadaDados = new FachadaDados();
+	private Perfil perfil = new Perfil();
+	
+	public void criarUsuario(String login, String senha, String nome,
+			String endereco, String email) throws Exception {
+
+		pessoa = new Usuario(login, senha, nome, endereco, email);
+	}
+
+	public void criarUsuario(String login, String nome, String endereco,
+			String email) throws Exception {
+		
+		pessoa = new Usuario(login, nome, endereco, email);
+
+	}
+
+	public void zerarSistema() {
+		fachadaDados.zerarSistema();
+
+	}
+
+	public String getAtributoUsuario(String login, String atributo)
+			throws Exception {
+		return fachadaDados.getAtributoUsuario(login, atributo);
+	}
+	
+	public String getAtributoCarona(int idCarona, String atributo) throws Exception {
+		return fachadaDados.getAtributoCarona(idCarona, atributo);
+	}
+	
+	public boolean isLoginCadastrado(String login) throws Exception {
+		return fachadaDados.isLoginCadastrado(login);
+	}
+
+	public boolean isSenhaValida(String login, String senha) throws Exception {
+		return fachadaDados.isSenhaValida(login, senha);
+	}
+
+	public String localizarCarona(int idCarona, String origem, String destino) throws Exception {
+		return fachadaDados.localizarCarona(idCarona, origem, destino);
+	}
+
+	public int cadastrarCarona(String idSessao,String localOrigem, String localDestino, String data, String horaDaSaida, int vagasDisponiveis) throws Exception {
+		return pessoa.cadastrarCarona(idSessao,localOrigem, localDestino, data, horaDaSaida, vagasDisponiveis);
+	}
+
+	public int getLinhasArquivo() throws Exception {
+		return fachadaDados.getLinhasArquivo();
+	}
+
+	public String getTrajeto(int idCarona) throws Exception {
+		return fachadaDados.getTrajeto(idCarona);
+	}
+
+	public int abrirSessao(String login, String senha) throws Exception {
+		return perfil.abrirSessao(login, senha);
+	}
+
+	public String getCarona(int idCarona) throws Exception {
+		return fachadaDados.getCarona(idCarona);
+	}
+
+	public boolean encerrarSistema() {
+		return perfil.encerrarSistema();
+	}
+
+}
